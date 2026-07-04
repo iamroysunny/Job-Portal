bfdcde29eeb3823f0e57fadda817d65ec09c8ea2
Sunny Roy <royofficial2@gmail.com>
2026-07-04 15:57:34 +0530

    Fix SalaryRangeSlider import path

--- frontend/src/pages/Jobseeker/components/FilterContent.jsx
+++ frontend/src/pages/Jobseeker/components/FilterContent.jsx
@@ -3,7 +3,7 @@ import {
   ChevronUp,
 } from "lucide-react";
 import { CATEGORIES, JOB_TYPES } from "../../../utils/data";
-import SalaryRangeSlider from "../../../../components/Input/salaryRangeSlider";
+import SalaryRangeSlider from "../../../components/Input/SalaryRangeSlider";
 const FilterSection = ({
   title,
   children,
